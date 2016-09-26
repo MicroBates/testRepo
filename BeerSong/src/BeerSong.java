b6e1e04fc535a21e5b15fb6b65ba84f2140c8445
MicroBates <ultra.bates@gmail.com>
2016-09-26 17:03:14 +0200

    Deuxième commit avec l'ajout de commentaires sur le code source

--- BeerSong/src/BeerSong.java
+++ BeerSong/src/BeerSong.java
@@ -2,20 +2,23 @@
 public class BeerSong {
 
 	public static void main(String[] args) {
+		//init et déclaration de variables
 		int beerNum = 99;
 		String word ="bottles";
-
+		
+		//boucle de traitement
 		while (beerNum > 0) {
-			
+			//traitement
 			System.out.println(beerNum + " " + word + " of beer on the wall");
 			System.out.println(beerNum + " " + word + " of beer.");
 			System.out.println("Take one down, Pass it around.");
+			//changement de valeur de variable
 			beerNum = beerNum - 1;
-			
+			//condition sur la valeur de la variable modifiée
 			if (beerNum == 1) {
 				word = "bottle";
 			}
-
+			//condition sur la valeur de la variable modifiée avant sous-traitement
 			if (beerNum > 0) {
 				System.out.println(beerNum + " " + word + " of beer on the wall");
 				System.out.println("");
